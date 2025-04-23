93380a3515cd6da7048e7d4975319be9bde44508
UserIsMissing <caschrei@ucsc.edu>
2025-04-22 19:22:50 -0700

    tiny changes to main

--- Wet-Dry-Cycler/src/main.c
+++ Wet-Dry-Cycler/src/main.c
@@ -19,7 +19,7 @@
 #include <main.h>
 
 // ***************************************************************
-/** 
+/**
  * @function HAL_GPIO_EXTI_Callback
  * @author Cole Schreiner
  * @date   22 Apr 2025
@@ -31,19 +31,29 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
     switch (GPIO_Pin)
     {
-    case GPIO_PIN_5: // PA5 bumper
+    // case GPIO_PIN_5: // PA5 bumper
+    case PIN_A5:          // PA5 bumper
+        BUMPER_STATE = 1; // Set bumper state to indicate left bumper hit
         printf("Left bumper hit!\r\n");
         MOVEMENT_Stop(); // Stop movement motor
         break;
-    case GPIO_PIN_6: // PA6 bumper
+    // case GPIO_PIN_6: // PA6 bumper
+    case PIN_A6:          // PA6 bumper
+        BUMPER_STATE = 2; // Set bumper state to indicate right bumper hit
         printf("Right bumper hit!\r\n");
         MOVEMENT_Stop(); // Stop movement motor
         break;
-    case GPIO_PIN_8: // PB8 Start Movement button
+    // case GPIO_PIN_8: // PB8 Start Movement button
+    case PIN_B8:          // PB8 Start Movement button
+        BUMPER_STATE = 3; // Set bumper state to indicate rear bumper hit
         printf("Rear bumper hit!\r\n");
         MOVEMENT_Move(); // Start movement motor
         break;
     }
+default:
+    // BUMPER_STATE = 0; // Reset bumper state if no bumper hit
+    printf("Unknown GPIO interrupt: %d\r\n", GPIO_Pin);
+    break;
 }
 
 /**
@@ -80,7 +90,6 @@ int main(void)
 
     while (1)
     {
-
     }
 }
 #endif // TESTING_MAIN
