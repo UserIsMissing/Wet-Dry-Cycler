448f8e4c880543e16a2b5e0c5d52c2eff86162c1
UserIsMissing <caschrei@ucsc.edu>
2025-05-16 13:18:22 -0700

    tiny changes. and recovery json worked?

--- Cycletron/src/main.cpp
+++ Cycletron/src/main.cpp
@@ -20,11 +20,11 @@
 // const char *ssid = "DonnaHouse";
 // const char *password = "guessthepassword";
 
-// const char *ssid = "TheDawgHouse";
-// const char *password = "ThrowItBackForPalestine";
+const char *ssid = "TheDawgHouse";
+const char *password = "ThrowItBackForPalestine";
 
-const char *ssid = "UCSC-Guest";
-const char* password = "";
+// const char *ssid = "UCSC-Guest";
+// const char* password = "";
 
 // === State Machine ===
 enum class SystemState
